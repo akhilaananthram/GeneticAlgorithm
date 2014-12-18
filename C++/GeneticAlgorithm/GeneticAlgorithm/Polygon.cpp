f1d18924b57f2dcb419f7f43f15d93d0caebc86b
Naman Agarwal <na298@cornell.edu>
2014-12-18 13:26:18 -0500

    removed spaces

--- C++/GeneticAlgorithm/GeneticAlgorithm/Polygon.cpp
+++ C++/GeneticAlgorithm/GeneticAlgorithm/Polygon.cpp
@@ -13,7 +13,11 @@ opacity(o)
 			center.x += p.x;
 			center.y += p.y;
 		}
-		center.x /= points.size();
-		center.y /= points.size();
-	
+		int sz = points.size();
+		center.x /= sz;
+		center.y /= sz;
+		for (int i = 0; i < sz; i++) {
+			float arctans[start_points];
+
+		}
 }
