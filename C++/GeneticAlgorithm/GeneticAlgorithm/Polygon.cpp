7a2b3ea61daadd6a05b571779f134f3b1151d33c
Naman Agarwal <na298@cornell.edu>
2014-12-19 18:37:38 -0500

    using externs in polygon to get the width and height values

--- C++/GeneticAlgorithm/GeneticAlgorithm/Polygon.cpp
+++ C++/GeneticAlgorithm/GeneticAlgorithm/Polygon.cpp
@@ -2,8 +2,12 @@
 #include <algorithm>
 #include <math.h>
 
+extern int WIDTH;
+extern int HEIGHT;
+/*
 #define WIDTH 100
 #define HEIGHT 100
+*/
 //todo figure out where these should go
 
 using namespace cv;
@@ -15,30 +19,30 @@ public:
 };
 Polygon::Polygon(int start_points)
 {
-		center = Point(0, 0);
-		for (int i = 0; i < start_points; i++) {
-			Point p = Point(rand() % WIDTH, rand() % HEIGHT);
-			points.push_back(p);
-			center.x += p.x;
-			center.y += p.y;
-		}
-		int sz = points.size();
-		center.x /= sz;
-		center.y /= sz;
-		SortHelper s;
-		s.center = center;
-		std::sort(points.begin(), points.end(), s);
-		red = rand() % 255;
-		blue = rand() % 255;
-		green = rand() % 255;
-		opacity = ((float)rand()) / RAND_MAX;
-		/*
-		float* arctans = new float[start_points];
-		for (int i = 0; i < sz; i++) {
-			Point p = points.at(i);
-			arctans[i] = atan2(p.y - center.y, p.x - center.x);
-		}
-		*/
+	center = Point(0, 0);
+	for (int i = 0; i < start_points; i++) {
+		Point p = Point(rand() % WIDTH, rand() % HEIGHT);
+		points.push_back(p);
+		center.x += p.x;
+		center.y += p.y;
+	}
+	int sz = points.size();
+	center.x /= sz;
+	center.y /= sz;
+	SortHelper s;
+	s.center = center;
+	std::sort(points.begin(), points.end(), s);
+	red = rand() % 255;
+	blue = rand() % 255;
+	green = rand() % 255;
+	opacity = ((float)rand()) / RAND_MAX;
+	/*
+	float* arctans = new float[start_points];
+	for (int i = 0; i < sz; i++) {
+		Point p = points.at(i);
+		arctans[i] = atan2(p.y - center.y, p.x - center.x);
+	}
+	*/
 }
 
 void Polygon::change_red() {
--- C++/GeneticAlgorithm/GeneticAlgorithm/main.cpp
+++ C++/GeneticAlgorithm/GeneticAlgorithm/main.cpp
@@ -5,11 +5,13 @@
 //  Created by Akhila Ananthram on 12/19/14.
 //  Copyright (c) 2014 Akhila Ananthram. All rights reserved.
 //
-
 #include <iostream>
+#include "Polygon.h"
+int WIDTH = 100, HEIGHT = 100;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+	Polygon p = Polygon();
     std::cout << "Hello, World!\n";
+	system("PAUSE");
     return 0;
 }
