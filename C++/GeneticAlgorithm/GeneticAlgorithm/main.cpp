bb7cc3c4e1a5904033920db7ab29f31b3c413833
Akhila Ananthram <asa225@cornell.edu>
2014-12-19 22:06:14 -0500

    finished local search

--- C++/GeneticAlgorithm/GeneticAlgorithm/main.cpp
+++ C++/GeneticAlgorithm/GeneticAlgorithm/main.cpp
@@ -1,11 +1,3 @@
-//
-//  main.cpp
-//  GeneticAlgorithm
-//
-//  Created by Akhila Ananthram on 12/19/14.
-//  Copyright (c) 2014 Akhila Ananthram. All rights reserved.
-//
-
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
